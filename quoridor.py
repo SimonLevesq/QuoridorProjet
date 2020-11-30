18423ae80aa710641644990dabdc829c8b9b5111
Joseph Ghanem <joseph.ghanem.1@ulaval.ca>
2020-11-30 14:34:36 -0500

    horizontal

--- quoridor.py
+++ quoridor.py
@@ -261,8 +261,8 @@ class Quoridor:
             return ('déplacer jeton', (x, y+1))
         
         if y <= Y:
-            Quoridor.placer_mur(self, joueur, (X-1, Y-1), 'vertical')
-            return ('placer mur vertical', (X-1, Y-1))
+            Quoridor.placer_mur(self, joueur, (X-1, Y-1), 'horizontal')
+            return ('placer mur horizontal', (X-1, Y-1))
 
         """Jouer un coup automatique pour un joueur.
         Pour le joueur spécifié, jouer automatiquement son meilleur coup pour l'état actuel
